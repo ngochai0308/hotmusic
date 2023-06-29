f92cbf88d6da13aaaa1ba97f0f3166f9ede5e016
NgocHai <gmNgocHai0308@gmail.com>
2023-06-29 17:24:30 +0700

    ..

---
+++ DataModel/AlbumSongs.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotMusic.DataModel
+{
+    [Table("AlbumSong")]
+    public class AlbumSongs
+    {
+        public int AlbumId { get; set; }
+        public int SongId { get; set; }
+        [NotMapped]
+        public string? AlbumTitle { get; set; }
+        [NotMapped]
+        public string? SongTitle { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public string? ModifiledBy { get; set; }
+        public bool? IsDeleted { get; set; }
+    }
+}
